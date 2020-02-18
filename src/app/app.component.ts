7d2a42f7703c4445c661c10f90ccfcffe0635e2f
KEALAN O CALLAGHAN - STUDENT <G00373834@gmit.ie>
2020-02-18 17:44:15 +0000

    lab4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'data-binding-app';
+
+count:number =0;
+hideLabel:boolean = true;
+
+
+buttonPressed(){
+  console.log("Button Pressed");
+  this.count++;
+  }
+
+
+imgClicked(){
+  console.log("Look at the star");
+ if(this.hideLabel == true) {
+   this.hideLabel = false;
+ }
+ else {
+   this.hideLabel = true
+  };
+ 
+}
+
+
 }
